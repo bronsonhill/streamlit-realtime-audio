50e91782ca49044f691f3d3d0ca8119069b4fa99
Bronson Hill <bronson.hill@yahoo.com.au>
2025-07-18 12:22:24 +1000

    modified:   requirements.txt modified:   setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="streamlit_realtime_audio",
-    version="0.0.5",
+    version="0.0.6",
     author="Bronson Hill",
     author_email="bronson.hill@yahoo.com.au",
     description="Streamlit component for real-time audio conversation with OpenAI using WebRTC",
